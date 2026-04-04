d6d4b19c32e169230f33cff890eac26cc3f6344a
alexzayac <azayac6@gmail.com>
2026-04-04 18:38:43 +0300

    chore (backend) setup ValidationPipe, helmet, cors in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common'
+import helmet from 'helmet'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
+    credentials: true,
+  })
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    })
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
